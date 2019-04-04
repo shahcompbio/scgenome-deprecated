ec671c1506147fe31630f505faf8fe50a340fd44
Andrew McPherson <andrew.mcpherson@gmail.com>
2019-04-04 20:30:08 +0000

    minor: temporary fix for importing from hdf5

--- scgenome/snvdata.py
+++ scgenome/snvdata.py
@@ -2,41 +2,40 @@ import os
 import wget
 import pandas as pd
 
+from datamanagement.miscellaneous.hdf5helper import read_python2_hdf5_dataframe
+
 
 def get_snv_results(dest):
     print('starting load')
 
-    mappability = pd.DataFrame()
-    for chunk in pd.read_hdf(dest, '/snv/mappability', chunksize=int(1e5)):
-        mappability = pd.concat([mappability, chunk[chunk['mappability'] > 0.99]], ignore_index=True)
+    mappability = read_python2_hdf5_dataframe(dest, '/snv/mappability')
+    mappability = mappability[mappability['mappability'] > 0.99]
     mappability['chrom'] = mappability['chrom'].astype(str)
 
-    store = pd.HDFStore(dest, 'r')
-
-    strelka_results = store['/strelka/vcf'].rename(columns={'score': 'strelka_score'})
+    strelka_results = read_python2_hdf5_dataframe(dest, '/strelka/vcf').rename(columns={'score': 'strelka_score'})
     print('strelka', strelka_results.shape)
     strelka_results = strelka_results[strelka_results['strelka_score'] > 20.]
     for col in ('chrom', 'ref', 'alt'):
         strelka_results[col] = strelka_results[col].astype(str)
 
-    museq_results = store['/museq/vcf'].rename(columns={'score': 'museq_score'})
+    museq_results = read_python2_hdf5_dataframe(dest, '/museq/vcf').rename(columns={'score': 'museq_score'})
     print('museq', museq_results.shape)
     museq_results = museq_results[museq_results['museq_score'] > .9]
     for col in ('chrom', 'ref', 'alt'):
         museq_results[col] = museq_results[col].astype(str)
 
-    cosmic = store['/snv/cosmic_status']
+    cosmic = read_python2_hdf5_dataframe(dest,'/snv/cosmic_status')
     cosmic['is_cosmic'] = True
     cosmic = cosmic[['chrom', 'coord', 'ref', 'alt', 'is_cosmic']].drop_duplicates()
 
-    snpeff = store['/snv/snpeff'][['chrom', 'coord', 'ref', 'alt', 'effect_impact']].drop_duplicates()
+    snpeff = read_python2_hdf5_dataframe(dest,'/snv/snpeff')[['chrom', 'coord', 'ref', 'alt', 'effect_impact']].drop_duplicates()
     snpeff['value'] = 1
     snpeff = snpeff.set_index(['chrom', 'coord', 'ref', 'alt', 'effect_impact'])['value'].unstack(fill_value=0)
     snpeff = snpeff.rename(columns=str).reset_index()
 
-    tnc = store['/snv/tri_nucleotide_context']
+    tnc = read_python2_hdf5_dataframe(dest,'/snv/tri_nucleotide_context')
 
-    data = store['/snv_allele_counts']
+    data = read_python2_hdf5_dataframe(dest, '/snv_allele_counts')
     print('total', data[['chrom', 'coord']].drop_duplicates().shape)
     data = data.merge(mappability)
     print('post mappability', data[['chrom', 'coord']].drop_duplicates().shape)
