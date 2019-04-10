87ea00ff4359bda1cc6e6a69fbdc8fdf3be0a7ed
Andrew McPherson <andrew.mcpherson@gmail.com>
2019-04-10 01:29:07 +0000

    snvs: select highest impact snpeff annotation

--- scgenome/snvdata.py
+++ scgenome/snvdata.py
@@ -5,6 +5,26 @@ import pandas as pd
 from datamanagement.miscellaneous.hdf5helper import read_python2_hdf5_dataframe
 
 
+def get_highest_snpeff_effect(snpeff_data):
+    """ Select the highest ranked effect from snpeff data.
+    """
+    print(snpeff_data)
+    ordered_effect_impacts = ['HIGH', 'MODERATE', 'LOW', 'MODIFIER']
+    snpeff_data = snpeff_data.merge(
+        pd.DataFrame({
+            'effect_impact': ordered_effect_impacts,
+            'effect_impact_rank': range(len(ordered_effect_impacts))}))
+    print(snpeff_data)
+    snpeff_data = (
+        snpeff_data.sort_values(['chrom', 'coord', 'ref', 'alt', 'effect_impact_rank'], ascending=True)
+        .groupby(['chrom', 'coord', 'ref', 'alt'], sort=False)
+        .first()
+        .reset_index())
+    print(snpeff_data)
+    snpeff_data = snpeff_data[['chrom', 'coord', 'ref', 'alt', 'gene_name', 'effect', 'effect_impact']]
+    return snpeff_data
+
+
 def get_snv_results(dest):
     print('starting load')
 
@@ -25,13 +45,13 @@ def get_snv_results(dest):
         museq_results[col] = museq_results[col].astype(str)
 
     cosmic = read_python2_hdf5_dataframe(dest,'/snv/cosmic_status')
+    print('cosmic', cosmic.shape)
     cosmic['is_cosmic'] = True
     cosmic = cosmic[['chrom', 'coord', 'ref', 'alt', 'is_cosmic']].drop_duplicates()
 
-    snpeff = read_python2_hdf5_dataframe(dest,'/snv/snpeff')[['chrom', 'coord', 'ref', 'alt', 'effect_impact']].drop_duplicates()
-    snpeff['value'] = 1
-    snpeff = snpeff.set_index(['chrom', 'coord', 'ref', 'alt', 'effect_impact'])['value'].unstack(fill_value=0)
-    snpeff = snpeff.rename(columns=str).reset_index()
+    snpeff = read_python2_hdf5_dataframe(dest,'/snv/snpeff')
+    print('snpeff', snpeff.shape)
+    snpeff = get_highest_snpeff_effect(snpeff)
 
     tnc = read_python2_hdf5_dataframe(dest,'/snv/tri_nucleotide_context')
 
@@ -39,9 +59,9 @@ def get_snv_results(dest):
     print('total', data[['chrom', 'coord']].drop_duplicates().shape)
     data = data.merge(mappability)
     print('post mappability', data[['chrom', 'coord']].drop_duplicates().shape)
-    data = data.merge(strelka_results)
+    data = data.merge(strelka_results, how='left')
     print('post strelka', data[['chrom', 'coord']].drop_duplicates().shape)
-    data = data.merge(museq_results)
+    data = data.merge(museq_results, how='left')
     print('post museq', data[['chrom', 'coord']].drop_duplicates().shape)
     data = data.merge(cosmic, how='left')
     print('post cosmic', data[['chrom', 'coord']].drop_duplicates().shape)
