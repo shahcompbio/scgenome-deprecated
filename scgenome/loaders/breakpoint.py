83aa4e011892ea2f83433e83c03e7c8498184587
Andrew McPherson <andrew.mcpherson@gmail.com>
2019-09-09 13:43:38 -0400

    refactor: haps loading and test load pseudobulk dtype check

--- scgenome/loaders/breakpoint.py
+++ scgenome/loaders/breakpoint.py
@@ -8,38 +8,6 @@ import scgenome.utils
 import scgenome.loaders.utils
 
 
-def get_pseudobulk_files(results_dir, suffix):
-    """ Get files for libraries and samples by suffix
-    
-    Args:
-        results_dir (str): pseudobulk results directory
-        suffix (str): suffix of requested files
-    
-    Yields:
-        (str, str, str): sample id, library id, filename
-    """
-    manifest_filename = os.path.join(results_dir, 'metadata.yaml')
-    manifest = yaml.load(open(manifest_filename))
-
-    tumour_samples = manifest['meta']['tumour_samples']
-    filenames = manifest['filenames']
-
-    for sample_info in tumour_samples:
-        sample_id = sample_info['sample_id']
-        library_id = sample_info['library_id']
-
-        sample_lib_suffix = f'{sample_id}_{library_id}_{suffix}'
-        sample_lib_filenames = list(filter(lambda a: a.endswith(sample_lib_suffix), filenames))
-
-        if len(sample_lib_filenames) != 1:
-            raise ValueError(f'found {len(sample_lib_filenames)} {suffix} files for {sample_id}, {library_id}, {results_dir}')
-
-        sample_lib_filename = sample_lib_filenames[0]
-        sample_lib_filepath = os.path.join(results_dir, sample_lib_filename)
-
-        yield sample_id, library_id, sample_lib_filepath
-
-
 def load_breakpoint_annotation_data(
         results_dir,
     ):
@@ -62,7 +30,7 @@ def load_breakpoint_annotation_data(
 
     breakpoint_data = []
 
-    for sample_id, library_id, filepath in get_pseudobulk_files(results_dir, suffix):
+    for sample_id, library_id, filepath in scgenome.loaders.utils.get_pseudobulk_files(results_dir, suffix):
         data = pd.read_csv(
             filepath, sep='\t', names=bpcols,
             dtype={
@@ -98,7 +66,7 @@ def load_breakpoint_count_data(
 
     breakpoint_count_data = []
 
-    for sample_id, library_id, filepath in get_pseudobulk_files(results_dir, suffix):
+    for sample_id, library_id, filepath in scgenome.loaders.utils.get_pseudobulk_files(results_dir, suffix):
         data = pd.read_csv(filepath, names=cols)
         data['library_id'] = library_id
         data['sample_id'] = sample_id
