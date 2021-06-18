e90f3edf53d629ffb0c71175c7449bc4abd48727
Andrew McPherson <andrew.mcpherson@gmail.com>
2021-06-18 15:09:15 -0400

    refactor: redo allele counts api

--- scgenome/loaders/allele.py
+++ scgenome/loaders/allele.py
@@ -6,6 +6,39 @@ import scgenome.loaders.utils
 import scgenome.utils
 
 
+def load_haplotype_allele_counts_from_results(results_dir):
+    """ Load haplotype allele counts from a results directory.
+
+    Args:
+        results_dir (str): results directory
+    """
+
+    allele_counts_filepath = scgenome.loaders.utils.find_results_filepath(
+        results_dir, 'allele_counts.csv.gz', analysis_type='count_haps')
+
+    return load_haplotype_allele_counts(allele_counts_filepath)
+
+
+def load_haplotype_allele_counts(allele_counts_filepath):
+    """ Load haplotype allele counts from a file.
+
+    Args:
+        allele_counts_filepath (str): results filepath
+    """
+
+    csv_input = scgenome.csvutils.CsvInput(allele_counts_filepath)
+    data = csv_input.read_csv(
+        dtypes_override={
+            'chromosome': 'category',
+            'cell_id': 'category',
+    })
+
+    return data
+
+
+# Deprecated from here
+# VVV
+
 def load_haplotype_allele_data(
         results_dir,
         filter_sample_id=None,
