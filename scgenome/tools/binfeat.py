948139c982b13decdbf6e06b2f51a350e79b14af
Andrew McPherson <andrew.mcpherson@gmail.com>
2022-02-09 21:18:28 -0500

    feature: read and bin (mean) bigwig data

--- scgenome/tools/binfeat.py
+++ scgenome/tools/binfeat.py
@@ -1,4 +1,6 @@
 import pyfaidx
+import pyBigWig
+import numba
 import pyranges as pr
 import numpy as np
 
@@ -69,6 +71,67 @@ def count_gc(bins, genome_fasta, column_name='gc', proportion=False):
 
     if proportion:
         data = data.assign(column_name, lambda df: df[column_name] / (df['End'] - df['Start']))
+
+    return data
+
+
+@numba.jit(nopython=True)
+def mean_ranges(values, sequence, starts, ends):
+    for i in range(starts.shape[0]):
+        values[i] = np.nanmean(sequence[starts[i]:ends[i]])
+
+
+def _chromosome_mean_bigwig(data, **kwargs):
+    bigwig_file = kwargs['bigwig_file']
+    chromosome = kwargs['chromosome']
+    column_name = kwargs['column_name']
+    chr_prefix = kwargs['chr_prefix']
+
+    chromosome = chr_prefix + chromosome
+    
+    bw = pyBigWig.open(bigwig_file, 'r')
+
+    if chromosome not in bw.chroms():
+        data[column_name] = None
+        return data
+
+    chromosome_length = bw.chroms()[chromosome]
+    sequence = np.array(bw.values(chromosome, start=0, end=chromosome_length))
+
+    x = np.zeros(data.shape[0])
+    mean_ranges(x, sequence, data['Start'].values, data['End'].values)
+
+    data[column_name] = x
+
+    return data
+
+
+def mean_from_bigwig(bins, bigwig_file, column_name, chr_prefix=''):
+    """ Count gc in each bin
+
+    Parameters
+    ----------
+    bins : pyranges.PyRanges
+        ranges for which to count gc
+    bigwig_file : str
+        bigwig filename
+    column_name : str
+        column to add to `bins`
+    chr_prefix : str
+        prefix for chromosome names, default ''
+
+    Returns
+    -------
+    pyranges.PyRanges
+        output ranges with additional column for mean bigwig value per bin
+    """    
+
+    data = bins.apply(
+        _chromosome_mean_bigwig,
+        bigwig_file=bigwig_file,
+        column_name=column_name,
+        chr_prefix=chr_prefix,
+    )
 
     return data
 
