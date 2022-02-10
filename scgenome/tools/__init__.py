2fb2736b2354c7754203ff2a2a59b7b7c8757a8a
Andrew McPherson <andrew.mcpherson@gmail.com>
2022-02-09 21:43:15 -0500

    minor: scgenome tools import

--- scgenome/tools/__init__.py
+++ scgenome/tools/__init__.py
@@ -2,6 +2,6 @@
 from .cluster import cluster_cells, cluster_cells_kmeans, aggregate_clusters_hmmcopy
 from .pca import pca_loadings
 from .sorting import sort_cells
-from .binfeat import create_bins, count_gc
+from .binfeat import create_bins, count_gc, mean_from_bigwig
 from .genes import read_ensemble_genes_gtf, aggregate_genes
 
