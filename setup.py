352e1b0cb137a7465b9780351fd5191c5d9de840
Andrew McPherson <andrew.mcpherson@gmail.com>
2022-06-16 11:24:19 -0400

    build: setup.py with dependencies

--- setup.py
+++ setup.py
@@ -1,14 +1,47 @@
 from setuptools import setup, find_packages
 
-setup(name='scgenome',
-      version='0.0.1',
-      description='Code for analyzing single cell genomes',
-      author='Shah Lab',
-      url='https://www.shahlab.ca/',
-      packages=find_packages(),
-      entry_points={
-        'console_scripts': [
-        ]
-      },
-      package_data={'':['data/mm10.fa.fai', 'data/GRCh37-lite.fa.fai', 'dtypes/*.yaml']},
-    )
+setup(
+    name='scgenome',
+    version='0.0.1',
+    description='Code for analyzing single cell whole genomes',
+    author='Shah Lab',
+    url='https://github.com/shahcompbio/scgenome',
+    packages=find_packages(),
+    install_requires=[
+        'anndata',
+        'bamread',
+        'bokeh',
+        'brewer2mpl',
+        'Click',
+        'csverve',
+        'hdbscan',
+        'hmmlearn',
+        'ipython',
+        'jupyter',
+        'lda',
+        'matplotlib',
+        'numba',
+        'numexpr',
+        'numpy',
+        'oauthlib',
+        'pandas',
+        'pyfaidx',
+        'pyranges',
+        'pysam',
+        'PyVCF',
+        'PyYAML',
+        'scikit-learn',
+        'scipy',
+        'seaborn',
+        'statsmodels',
+        'umap-learn',
+        'wgs_analysis @ git+https://github.com/amcpherson/wgs_analysis.git#egg=wgs_analysis',
+    ],
+    package_data={
+        'scgenome': [
+            'data/mm10.fa.fai',
+            'data/GRCh37-lite.fa.fai',
+            'dtypes/*.yaml',
+        ],
+    },
+)
