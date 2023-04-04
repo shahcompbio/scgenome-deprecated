6d7a75a18e5c00a1d1ca0ebaa1c2c41a3bb6e54f
Andrew McPherson <andrew.mcpherson@gmail.com>
2023-04-04 19:03:32 -0400

    feature: get obs data function

---
+++ scgenome/tools/getters.py
@@ -0,0 +1,44 @@
+from anndata import AnnData
+from pandas import DataFrame
+
+
+def get_obs_data(
+        adata: AnnData,
+        obs_id: str,
+        var_columns=None,
+        layer_names=None) -> DataFrame:
+    """ Get a dataframe for an observation including .var and specified layers
+
+    Parameters
+    ----------
+    adata : AnnData
+        anndata from which to retrieve observation data.
+    obs_id : str
+        Observation ID
+    var_columns : list, optional
+        List of var columns to add to returned dataframe, by default None, all columns
+    layer_names : list, optional
+        List of layer names to add to returned dataframe, by default None, all layers
+
+    Returns
+    -------
+    DataFrame
+        dataframe for an observation
+    """
+    data = adata.var
+
+    if var_columns is not None:
+        data = data[var_columns]
+
+    if layer_names is None:
+        layer_names = adata.layers.keys()
+
+    for layer_name in layer_names:
+        assert layer_name not in data, f'layer {layer_name} also in .var'
+        layer_data = adata[obs_id].to_df(layer_name).T
+        assert len(layer_data.columns) == 1
+        layer_data.columns = [layer_name]
+        data = data.merge(layer_data, left_index=True, right_index=True, how='left')
+    
+    return data
+
