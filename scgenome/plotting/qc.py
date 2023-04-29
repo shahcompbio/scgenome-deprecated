aebd7c1606d253857e5ebd93cef161c1a074b40f
Andrew McPherson <andrew.mcpherson@gmail.com>
2023-04-29 09:54:30 -0400

    minor: gc plotting

--- scgenome/plotting/qc.py
+++ scgenome/plotting/qc.py
@@ -1,6 +1,9 @@
+import seaborn as sns
+
+import scgenome.tools.getters
 
 
-def plot_gc_reads(adata, obs_id, *args, **kwargs):
+def plot_gc_reads(adata, obs_id):
     """ Plot scatter points of gc by read count.
 
     Args:
@@ -8,4 +11,8 @@ def plot_gc_reads(adata, obs_id, *args, **kwargs):
         obs_id (str): cell or clone to plot
     """
 
-    raise NotImplementedError
+    data = scgenome.tools.getters.get_obs_data(adata, cell_id, var_columns=['gc'], layer_names=[None])
+    data = data.rename(columns={'_X': 'reads'})
+    data = data[data['gc'] > 0]
+
+    sns.scatterplot(x='gc', y='reads', data=data, s=10)
