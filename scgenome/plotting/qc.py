eef1e5dc33a2151b2cd0d142fe328a428d968314
Andrew McPherson <andrew.mcpherson@gmail.com>
2023-10-02 20:05:32 -0400

    minor: kwargs for gc reads scatter

--- scgenome/plotting/qc.py
+++ scgenome/plotting/qc.py
@@ -3,16 +3,19 @@ import seaborn as sns
 import scgenome.tools.getters
 
 
-def plot_gc_reads(adata, obs_id):
+def plot_gc_reads(adata, obs_id, **kwargs):
     """ Plot scatter points of gc by read count.
 
     Args:
         adata (anndata.AnnData): copy number
         obs_id (str): cell or clone to plot
+    
+    KwArgs:
+        **kwargs: passed to sns.scatterplot
     """
 
     data = scgenome.tools.getters.get_obs_data(adata, obs_id, var_columns=['gc'], layer_names=[None])
     data = data.rename(columns={'_X': 'reads'})
     data = data[data['gc'] > 0]
 
-    sns.scatterplot(x='gc', y='reads', data=data, s=10)
+    sns.scatterplot(x='gc', y='reads', data=data, **kwargs)
